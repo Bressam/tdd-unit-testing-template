1e469c6e334dbb0eab756e7dcb09cb8f9febc87f
Giovanne Bressam <giovannebressam@gmail.com>
2021-12-12 18:48:15 -0300

    Move finished class to production environment

--- UnitTest&TDD-EssentialDeveloperCrashCourse/CrashCourseTests/FriendsTest2TDDExample.swift
+++ UnitTest&TDD-EssentialDeveloperCrashCourse/CrashCourseTests/FriendsTest2TDDExample.swift
@@ -38,90 +38,6 @@ import XCTest
             * If a retry succeeds: show friends
     * On selection: show friend detail
  */
-class FriendsViewController: UITableViewController {
-    private let friendsService: FriendsAPI
-    private var friends: [Friend] = [] {
-        didSet {
-            tableView.reloadData()
-        }
-    }
-    
-    // D.I. Injection instead of legacy code on other example that we needed to change the service to a public var to inject
-    init(friendsService: FriendsAPI) {
-        self.friendsService = friendsService
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    // hide required init that will never be used
-    @available (*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        self.loadFirendsWithRetry()
-    }
-    
-    func loadFirendsWithRetry(retryCount: Int = 0) {
-        friendsService.loadFriends { [weak self] friendsResult in
-            switch friendsResult {
-            case let .success(friends):
-                self?.friends = friends
-            case let .failure(error):
-                if retryCount == 2 {
-                    self?.show(error)
-                } else {
-                    self?.loadFirendsWithRetry(retryCount: retryCount+1)
-                }
-            }
-        }
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.friends.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let tableviewCell = tableView.dequeueReusableCell(withIdentifier: "Cell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
-        let friend = self.friends[indexPath.row]
-        tableviewCell.textLabel?.text = friend.name
-        tableviewCell.detailTextLabel?.text = friend.phone
-        return tableviewCell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        show(self.friends[indexPath.row])
-    }
-    
-}
-
-
-// MARK: Service Test Doubles
-/// Holds count of api calls to check lifecycle
-private class FriendsServiceSpy: FriendsAPI {
-    private(set) var loadFriendsCount = 0
-    private var results: [Result<[Friend], Error>]
-    
-    init(result: [Friend] = []) {
-        self.results = [.success(result)]
-    }
-    
-    init(results: [Result<[Friend], Error>]) {
-        self.results = results
-    }
-    
-    override func loadFriends(completion: @escaping (Result<[Friend], Error>) -> Void) {
-        loadFriendsCount += 1
-        // removeFirst pops the element and return it
-        completion(results.removeFirst())
-    }
-}
-
 
 //MARK: Test
 class FriendsTDDTests: XCTestCase {
@@ -241,6 +157,27 @@ class FriendsTDDTests: XCTestCase {
         let detailView = navigation.topViewController as? FriendDetailsViewController
         XCTAssertEqual(detailView?.friend, friend)
         
+    }
+}
+
+// MARK: Service Test Doubles
+/// Holds count of api calls to check lifecycle
+private class FriendsServiceSpy: FriendsAPI {
+    private(set) var loadFriendsCount = 0
+    private var results: [Result<[Friend], Error>]
+    
+    init(result: [Friend] = []) {
+        self.results = [.success(result)]
+    }
+    
+    init(results: [Result<[Friend], Error>]) {
+        self.results = results
+    }
+    
+    override func loadFriends(completion: @escaping (Result<[Friend], Error>) -> Void) {
+        loadFriendsCount += 1
+        // removeFirst pops the element and return it
+        completion(results.removeFirst())
     }
 }
 
